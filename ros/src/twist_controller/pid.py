5b5b732dae0221f4099a5ee1c6d9d858b75d99b1
W.Lin <43103648+copperbeancheng@users.noreply.github.com>
2019-05-29 21:06:18 +0900

    Update pid.py

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -15,13 +15,18 @@ class PID(object):
 
     def reset(self):
         self.int_val = 0.0
+        self.last_int_val = 0.0
+        self.last_error = 0.0
 
     def step(self, error, sample_time):
+        self.last_int_val = self.int_val
 
         integral = self.int_val + error * sample_time;
         derivative = (error - self.last_error) / sample_time;
 
-        val = self.kp * error + self.ki * integral + self.kd * derivative;
+        y = self.kp * error + self.ki * self.int_val + self.kd * derivative
+        val = max(self.min, min(y, self.max))
+        #val = self.kp * error + self.ki * integral + self.kd * derivative;
 
         if val > self.max:
             val = self.max
